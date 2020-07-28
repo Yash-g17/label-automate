8e27c44cc32efd0ca97716e97e6f5093093fa0d8
Yash-g17 <yg2520175@gmail.com>
2020-07-28 23:09:09 +0530

    front-end dev

--- qrcode-custom.js
+++ qrcode-custom.js
@@ -9,29 +9,20 @@ let qrcode = new QRCode("output", {
 
 let a;
 function httpGet(theUrl) {
-  console.log("inside httpGet ====> " + theUrl);
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", theUrl); // false for synchronous request
   xmlHttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      console.log("inside onreadtstatechange");
       a = xmlHttp.responseText;
       qrcode.clear(); // clear the code.
       qrcode.makeCode(a); // make another code.
-      // a = xmlHttp.responseText;
-      // return xmlHttp.responseText;
-      // console.log("respomse ===> " + xmlHttp.responseText);
-    } else {
-      // return xmlHttp.responseText;
+      document.getElementById("content").innerHTML = a;
     }
   };
   xmlHttp.send();
-  console.log("sending the req.");
   // return xmlHttp.responseText;
   // fetch("theUrl").then((response) => console.log(response.text()));
 }
 document.getElementById("button").addEventListener("click", () => {
-  console.log("button clicked");
-
   httpGet("http://127.0.0.1:3000/api");
 });
