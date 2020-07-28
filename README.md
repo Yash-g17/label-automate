f8836758440ac9a8947a309a0e02f2f4c572b0c3
Yash-g17 <yg2520175@gmail.com>
2020-07-28 15:36:21 +0530

    done with the backend

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # label-automate
+
 label automater for small businesses.
+run the node application app.js,
+This application=>
+->tails the log file and reads new changes
+->creates an api which sends new date and time as the response.
+after running the node application open label.html,
+This webpage=>
+->uses qrcode-custom.js to send a GET req to fetch the current date and time and create its qr code using davids qr-code library
+Use this to tail any file and generate qr-codes when necessary.
--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-const readLastLines = require("read-last-lines");
-readLastLines.read("path/to/file", 1).then((lines) => console.log(lines));
+Tail = require("tail").Tail;
+const express = require("express");
+const app = express();
+tail = new Tail("./log");
+let a;
+tail.on("line", function (data) {
+  a = data;
+  console.log(data);
+});
+
+tail.on("error", function (error) {
+  console.log("ERROR: ", error);
+});
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
+app.get("/api", (req, res) => {
+  console.log("output ==========>" + a);
+  res.send(a);
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
