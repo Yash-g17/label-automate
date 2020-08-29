fdd957a57de2a5ba88ff867277deda1b41139961
Yash-g17 <yg2520175@gmail.com>
2020-08-29 23:16:06 +0530

    created a new repository to store dispatch app

--- dispatch-client.js
+++
@@ -1,5 +0,0 @@
-var socket = io.connect("http://localhost:5000");
-socket.on("code", (data) => {
-    console.log("code recieved" + data);
-    //   document.querySelector("#text-2").innerHTML = data;
-});
